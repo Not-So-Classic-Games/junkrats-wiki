b7c85b6895fea8a88c6a459fdd1326904490b84f
Not-So Classic Games CI <ci@notsoclassicgames.com>
2026-03-11 11:22:12 +0000

    Auto build wiki

--- README.md
+++
@@ -1,2 +0,0 @@
-# junkrats-wiki
-Official JunkRats Wiki maintained by Not-So Classic Games.
--- site/assets/javascripts/extra.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("JunkRats Wiki custom theme loaded.");
-});
